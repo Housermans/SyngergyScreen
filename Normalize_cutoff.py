ee37581986322109a74585a73ed166ee27d81258
Housermans <mahuismans@gmail.com>
2021-05-06 18:48:44 +0200

    Nu een manier gemaakt om verschil tussen pos en neg ctrl te plotten! Signed-off-by: Housermans <mahuismans@gmail.com>
    Signed-off-by: Housermans <mahuismans@gmail.com>

--- Normalize_cutoff.py
+++ Normalize_cutoff.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 exp_df = pd.read_csv('output/experiment_table.csv', sep=';')
 
-def select_condition(select_id, maxsize=50000, plot=False, title='NA'):
+def select_condition(select_id, maxsize=50000, plot=False, title='NA', drop=[]):
     select_df = exp_df.loc[exp_df['cond_id'] == select_id]
     cond_name = select_df['Cond_name'].unique()[0]
     organoid_df = pd.DataFrame(columns=['Unnamed: 0', 'ScreenName', 'ScreenID', 'PlateName', 'PlateID',
@@ -13,26 +13,47 @@ def select_condition(select_id, maxsize=50000, plot=False, title='NA'):
     for index, row in select_df.iterrows():
         temp_df = pd.read_csv(row['file_path'], sep=';')
         organoid_df = pd.concat([organoid_df, temp_df])
+    # reset index omdat je verschillende dataframes aan elkaar hebt geplakt, anders krijg je rare uitkomsten met drop
+    organoid_df.reset_index(inplace=True)
     organoid_df = organoid_df.drop(organoid_df[organoid_df.cell_area > maxsize].index)
+    if drop:
+        for item in drop:
+            # print('dropping', item)
+            if not item in organoid_df['WellName'].unique():
+                # print(f'{item} not found')
+                return None
+            organoid_df = organoid_df.drop(organoid_df[organoid_df.WellName == item].index)
+    # if argument plot is set as true in function call, two summary plots are created for this condition
+    # print(organoid_df['WellName'].unique())
     if plot:
         if (title == 'NA'):
             title = cond_name
         summarize_condition(organoid_df, title=title)
-        summarize_per_well(organoid_df, title=title)
+        summarize_per_well(organoid_df, title=title, drop=drop)
     return organoid_df
 
-def summarize_per_well(df, title="Organoids"):
+def summarize_per_well(df, title="Organoids", drop=[]):
     wells = df['WellName'].unique()
-    n_wells = len(wells)
+    n_wells = len(wells) + len(drop)
     rows = int(n_wells / 8)
     columns = 8
     count = 1
     plt.subplots(rows, columns, sharey='row', sharex='col')
     for well in wells:
+        if well in drop:
+            ax = plt.subplot(rows, columns, count)
+            plt.plot([])
+            plt.title(f"{well} blinded")
+            plt.xlim(0, 1)
+            ax.set_xticks([0.5])
+            ax.set_xticklabels([0.5])
+            count += 1
+            continue
         well_df = df[df['WellName'] == well]
         ax = plt.subplot(rows,columns,count)
         plt.title(well)
         plt.hist(well_df['cell_round'], density=True, stacked=True)
+        plt.xlim(0,1)
         ax.set_xticks([0.5])
         ax.set_xticklabels([0.5])
         count += 1
@@ -54,13 +75,49 @@ def summarize_condition(df, title='Organoids'):
     fig.suptitle(title)
     plt.show()
 
-vino120_organoids_df = select_condition(10, plot=True, title="vinorelbine 120")
-negctrl_organoid_df = select_condition(1, plot=True)
-# posctrl_summary_df, posctrl_organoid_df = select_condition(2)
+def classify_alive(df, cut_off):
+    cut_off = cut_off/100
+    alive = 0
+    for index, row in df.iterrows():
+        if row['cell_round'] > cut_off:
+            alive += 1
+    return alive/len(df)
+
+def plt_cutoff(df, lower, upper, plot=False):
+    lower = int(lower*100)
+    upper = int(upper*100)
+    pct_alive = []
+    for value in range(lower, upper+1, 1):
+        pct_alive.append(classify_alive(df, value))
+    if plot:
+        plt.bar(range(lower, upper+1, 1), pct_alive)
+        plt.show()
+    return pct_alive
+
+def plt_diff(neg_df, pos_df, lower, upper):
+    neg = plt_cutoff(neg_df, lower, upper)
+    pos = plt_cutoff(pos_df, lower, upper)
+    lower = int(lower * 100)
+    upper = int(upper * 100)
+    diff = []
+    for i in range(len(neg)):
+        diff.append(neg[i] - pos[i])
+    plt.bar(range(lower, upper+1, 1), diff)
+    plt.show()
+    return diff
+
+negctrl_organoid_df = select_condition(1)
+posctrl_organoid_df = select_condition(2, drop=['K22'])
+plt_diff(negctrl_organoid_df, posctrl_organoid_df, 0.5, 0.9)
 # summarize_condition(negctrl_organoid_df1, title="Negative Control (DMSO)")
 # summarize_condition(posctrl_organoid_df2, title="Positive control (Navitoclax 10microM)")
 # summarize_per_well(negctrl_organoid_df, title='negative control (DMSO)')
 # summarize_per_well(posctrl_organoid_df, title='positive control (navitoclax)')
+
+# print(classify_alive(negctrl_organoid_df, 70))
+# neg = plt_cutoff(negctrl_organoid_df, 0.5, 0.9)
+# pos = plt_cutoff(posctrl_organoid_df, 0.5, 0.9)
+
 
 '''
 print(f"Negative control: {len(negctrl_summary_df)}")
