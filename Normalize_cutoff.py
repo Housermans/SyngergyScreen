bf77451d521db77fbad9e757cc51306aa83ad833
Housermans <mahuismans@gmail.com>
2021-05-06 17:23:07 +0200

    Normalize_cutoff.py toegevoegd waar je plotjes kan maken om condities te bekijken. Idee is om hier de cut_off value te gaan berekenen.
    Signed-off-by: Housermans <mahuismans@gmail.com>

--- Normalize_cutoff.py
+++ Normalize_cutoff.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 
 exp_df = pd.read_csv('output/experiment_table.csv', sep=';')
 
-def select_condition(select_id):
+def select_condition(select_id, maxsize=50000, plot=False, title='NA'):
     select_df = exp_df.loc[exp_df['cond_id'] == select_id]
+    cond_name = select_df['Cond_name'].unique()[0]
     organoid_df = pd.DataFrame(columns=['Unnamed: 0', 'ScreenName', 'ScreenID', 'PlateName', 'PlateID',
        'MeasurementDate', 'MeasurementID', 'WellName', 'Row', 'Column',
        'Field', 'Timepoint', 'Object Number', 'X', 'Y', 'Bounding Box',
@@ -12,14 +13,55 @@ def select_condition(select_id):
     for index, row in select_df.iterrows():
         temp_df = pd.read_csv(row['file_path'], sep=';')
         organoid_df = pd.concat([organoid_df, temp_df])
-    return select_df, organoid_df
+    organoid_df = organoid_df.drop(organoid_df[organoid_df.cell_area > maxsize].index)
+    if plot:
+        if (title == 'NA'):
+            title = cond_name
+        summarize_condition(organoid_df, title=title)
+        summarize_per_well(organoid_df, title=title)
+    return organoid_df
 
-negctrl_summary_df, negctrl_organoid_df1 = select_condition(1)
-posctrl_summary_df, posctrl_organoid_df2 = select_condition(2)
+def summarize_per_well(df, title="Organoids"):
+    wells = df['WellName'].unique()
+    n_wells = len(wells)
+    rows = int(n_wells / 8)
+    columns = 8
+    count = 1
+    plt.subplots(rows, columns, sharey='row', sharex='col')
+    for well in wells:
+        well_df = df[df['WellName'] == well]
+        ax = plt.subplot(rows,columns,count)
+        plt.title(well)
+        plt.hist(well_df['cell_round'], density=True, stacked=True)
+        ax.set_xticks([0.5])
+        ax.set_xticklabels([0.5])
+        count += 1
+    plt.subplots_adjust(wspace=0.25)
+    plt.suptitle(title)
+    plt.show()
 
-print(negctrl_organoid_df1)
-plt.hist(negctrl_organoid_df1['cell_round'])
-plt.show()
+def summarize_condition(df, title='Organoids'):
+    fig=plt.figure(figsize=(10,8))
+    plt.subplot(121)
+    plt.hist(df['cell_round'])
+    plt.xlabel('Roundness')
+    plt.ylabel('Count')
+    plt.subplot(122)
+    plt.scatter(df['cell_round'], df['cell_area'])
+    plt.xlabel('Roundness')
+    plt.ylabel('Area')
+    plt.subplots_adjust(wspace=0.35)
+    fig.suptitle(title)
+    plt.show()
+
+vino120_organoids_df = select_condition(10, plot=True, title="vinorelbine 120")
+negctrl_organoid_df = select_condition(1, plot=True)
+# posctrl_summary_df, posctrl_organoid_df = select_condition(2)
+# summarize_condition(negctrl_organoid_df1, title="Negative Control (DMSO)")
+# summarize_condition(posctrl_organoid_df2, title="Positive control (Navitoclax 10microM)")
+# summarize_per_well(negctrl_organoid_df, title='negative control (DMSO)')
+# summarize_per_well(posctrl_organoid_df, title='positive control (navitoclax)')
+
 '''
 print(f"Negative control: {len(negctrl_summary_df)}")
 print(negctrl_summary_df['mean_roundness'])
