44b9a7e39180e038c072aa4e485859f6f42ecbfc
Housermans <mahuismans@gmail.com>
2021-08-04 16:53:22 +0200

    Standaard conditionstable gemaakt om te hergebruiken voor synergy screens. Signed-off-by: Housermans <mahuismans@gmail.com>
    Signed-off-by: Housermans <mahuismans@gmail.com>

--- script/Experiment_setup.py
+++ script/Experiment_setup.py
@@ -1,7 +1,8 @@
 import os
 from glob import glob
 
-experiment_folder = "E21-004_RAS13"
+
+experiment_folder = input('Give a name for the new drugscreen folder (don\'t use spaces)')
 
 def folder_set_up(experiment_folder):
     # input is the name of a experiment you want to analyse. Makes folders that are then used for the analysis.
@@ -15,6 +16,10 @@ def folder_set_up(experiment_folder):
         else:
             print(f"making {full_path}")
             os.mkdir(full_path)
+    use_stand_cond = input('Use Lapatinib/binimetinib/vinorelbine synergy screen condition table? (y/n)')
+    if use_stand_cond == 'y' or use_stand_cond == 'Y':
+        os.system(f'cp ../resources/Conditions_table_synergy.xlsx ../{experiment_folder}/input/Conditions_table.xlsx')
+        print(f'Added Conditions_table.xlsx to {experiment_folder}/input')
 
 def check_if_set(experiment_folder):
     necessary_files = ['input/well_csv/*.csv', 'input/Conditions_table.xlsx', 'input/Drugconcentrations_perwell.xlsx']
@@ -33,4 +38,5 @@ def check_if_set(experiment_folder):
         print("Something went wrong, make sure you used the correct names for the input folder and files.")
 
 folder_set_up(experiment_folder)
-check_if_set(experiment_folder)
+
+# check_if_set(experiment_folder)
