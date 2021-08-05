4ac22db3c70b3e21da80c1bc6e70976b49459f0c
Housermans <mahuismans@gmail.com>
2021-08-05 15:14:00 +0200

    Begin gemaakt aan notebook Signed-off-by: Housermans <mahuismans@gmail.com>
    Signed-off-by: Housermans <mahuismans@gmail.com>

--- script/Experiment_setup.py
+++ script/Experiment_setup.py
@@ -1,9 +1,6 @@
 import os
 from glob import glob
 
-
-experiment = input('Give a name for the new drugscreen folder (don\'t use spaces)')
-
 def folder_set_up(experiment):
     # input is the name of a experiment you want to analyse. Makes folders that are then used for the analysis.
     necessary_folders = ['','figures', 'input', 'input/well_csv', 'output',
@@ -16,10 +13,10 @@ def folder_set_up(experiment):
         else:
             print(f"making {full_path}")
             os.mkdir(full_path)
-    use_stand_cond = input('Use Lapatinib/binimetinib/vinorelbine synergy screen condition table? (y/n)')
-    if use_stand_cond == 'y' or use_stand_cond == 'Y':
-        os.system(f'cp ../resources/Conditions_table_synergy.xlsx ../data/{experiment}/input/Conditions_table.xlsx')
-        print(f'Added Conditions_table.xlsx to {experiment}/input')
+
+def use_synergy_table(experiment):
+    os.system(f'cp ../resources/Conditions_table_synergy.xlsx ../data/{experiment}/input/Conditions_table.xlsx')
+    print(f'Added Conditions_table.xlsx to {experiment}/input')
 
 def check_if_set(experiment):
     necessary_files = ['input/well_csv/*.csv', 'input/Conditions_table.xlsx', 'input/Drugconcentrations_perwell.xlsx']
@@ -37,7 +34,8 @@ def check_if_set(experiment):
     else:
         print("Something went wrong, make sure you used the correct names for the input folder and files.")
 
+# experiment = input('Give a name for the new drugscreen folder (don\'t use spaces)')
 
-folder_set_up(experiment)
+# folder_set_up(experiment)
 
 # check_if_set(experiment)
